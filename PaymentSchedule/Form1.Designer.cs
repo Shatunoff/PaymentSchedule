3f094b8d6b091fe170db5458453011b1a77ba8ea
tori690 <tori690@yandex.ru>
2020-04-24 18:30:10 +0300

    Добавление иконки

--- PaymentSchedule/Form1.Designer.cs
+++ PaymentSchedule/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.gbSchedule = new System.Windows.Forms.GroupBox();
             this.dgvSchedule = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -466,6 +467,7 @@
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.gbSchedule);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
